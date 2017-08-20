f180087cecfa2f01d5c586970367cfa2a6d79962
sosorry <sosorry@raspberrypi.com.tw>
2017-08-20 12:54:28 +0800

    1. change the year of Copyright from 2016 to 2017

--- camera-opencv/02-image_process/draw_moment.py
+++ camera-opencv/02-image_process/draw_moment.py
@@ -2,7 +2,7 @@
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 #|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# Copyright (c) 2016, raspberrypi.com.tw
+# Copyright (c) 2017, raspberrypi.com.tw
 # All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
